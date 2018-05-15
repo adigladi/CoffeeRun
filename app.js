fab550785cd565690678c10f7e7632beba133b46
Adrian Berger <adigladi@n171-p35.eduroam.kth.se>
2018-05-15 14:36:45 +0200

    Formatting

--- app.js
+++ app.js
@@ -447,10 +447,10 @@ function initMap() {
 
 function addNewMarker() {
 	markernr += 1;
-	var markername = "marker"+markernr;
-	var infomane = "infowindow"+markernr;
+	var markername = "marker" + markernr;
+	var infomane = "infowindow" + markernr;
 	var markerinfo = prompt("Description of the marker:");
-	$("#available").append("<ons-card id="+markername+"><div class='title'>"+markerinfo+"</div></ons-card>")
+	$("#available").append("<ons-card id=" + markername + "><div class='title'>" + markerinfo + "</div></ons-card>")
 	var infoname = new google.maps.InfoWindow({
 		content: markerinfo
 	});
@@ -478,7 +478,7 @@ window.fn.pushPage = function (page, anim) {
 	}
 };
 
-var requestClick = function() {
+var requestClick = function () {
 	if (document.getElementById('requestPage').style.display === 'none') {
 		showRequest();
 	}
@@ -487,22 +487,22 @@ var requestClick = function() {
 	}
 };
 
-var showRequest = function() {
+var showRequest = function () {
 	document.getElementById('requestPage').style.display = 'block';
 	document.getElementById('map').style.filter = 'blur(5px)';
 };
 
-var hideRequest = function() {
+var hideRequest = function () {
 	document.getElementById('requestPage').style.display = 'none';
 	document.getElementById('map').style.filter = 'blur(0px)';
 };
 
-var getOrder = function() {
+var getOrder = function () {
 	var navigator = document.getElementById('navigator');
 	navigator.pushPage('order.html');
 };
 
-var removeOrder = function() {
+var removeOrder = function () {
 	var navigator = document.getElementById('navigator');
 	navigator.popPage();
 };
@@ -511,27 +511,27 @@ var removeOrder = function() {
 //Order functions
 var orders = [];
 
-var placeOrder = function() {
+var placeOrder = function () {
 	var type = document.getElementById("coffeeType").value;
 	var place = document.getElementById("coffeePlace").value;
 	var number = document.getElementById("coffeeNumber").value;
 	var requestName = document.getElementById("requestName").value;
 	var addInfo = document.getElementById("addInfo").value;
-	
+
 	/*$("#available").append("<ons-card><div class='title'>"+ number + " " + type + " from " + place + "</div></ons-card>");
 	hideRequest(); */
-	
+
 	var rand = getId();
 
 	var arr = [type, place, number, requestName, addInfo];
-	var obj = {id: rand, order: arr};
+	var obj = { id: rand, order: arr };
 	orders.push(obj);
 	updateOrders();
 	console.log(orders);
 	requestClick();
 }
 
-var getId = function() {
+var getId = function () {
 	var ids = [];
 	for (var i; i < orders.length; i++) {
 		ids.push(orders[i].id);
@@ -544,10 +544,10 @@ var getId = function() {
 	return num;
 }
 
-var updateOrders = function() {
+var updateOrders = function () {
 	$("#available").html("");
 	for (var i = 0; i < orders.length; i++) {
-		$("#available").append("<ons-card id="+ orders[i].id +"><div class='title'>"+ orders[i].order[2] + " " + orders[i].order[0] + " from " + orders[i].order[1] + "</div></ons-card>");
+		$("#available").append("<ons-card id=" + orders[i].id + "><div class='title'>" + orders[i].order[2] + " " + orders[i].order[0] + " from " + orders[i].order[1] + "</div></ons-card>");
 	}
 }
 //End of Order functions
